75b4403069d502e1bc76c101deb0d0e45bfcb72f
KASHIF-SHOUKAT-SE <149269224+KASHIF-SHOUKAT-SE@users.noreply.github.com>
2024-04-24 11:29:46 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = prompt("hi i am google")
+a =Number.parseInt(a)
+if(a>0){
+    alert("this is a valid age")
+}
+else if(a<9){
+    alert("you are a kid")
+}
+else{
+    alert("this is invalid age")
+}
